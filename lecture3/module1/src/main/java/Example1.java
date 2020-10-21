55f3a36ccdd24ebf9de9884de16a60c03af88511
Dmitry <dimkagorbatencko@gmail.com>
2020-10-21 11:12:34 +0300

    info -> debug

--- lecture3/module1/src/main/java/Example1.java
+++ lecture3/module1/src/main/java/Example1.java
@@ -9,16 +9,15 @@ public class Example1 {
     public static void main(String[] args) {
 
         int a = 4;
-//        System.out.println("a = 4");
         logger.debug("a = {}", a);
 
         int b = 5;
-//        System.out.println("b = 5");
-        logger.info("b = {}", b);
+        logger.debug("b = {}", b);
 
         int result = a + b;
-//        System.out.println("a + b = " + result);
-        logger.info("a + b = {}", result);
+        logger.debug("a + b = {}", result);
 
+        User user = new User("di", "go");
+        logger.debug("created : {}", user);
     }
 }
